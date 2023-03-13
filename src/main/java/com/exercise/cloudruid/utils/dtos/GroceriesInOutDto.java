abad8f2e8645e3bfa89207133209fd4a8608e3b7
Radoslav <ketchup4eto@abv.bg>
2023-03-13 03:47:05 +0200

    Added an empty constructor to ease work with this class

--- src/main/java/com/exercise/cloudruid/utils/dtos/GroceriesInOutDto.java
+++ src/main/java/com/exercise/cloudruid/utils/dtos/GroceriesInOutDto.java
@@ -2,11 +2,13 @@ package com.exercise.cloudruid.utils.dtos;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.validation.constraints.NotEmpty;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Setter
 @Getter
 public class GroceriesInOutDto {
@@ -14,7 +16,7 @@ public class GroceriesInOutDto {
     @NotEmpty(message = "Product name cannot be empty!")
     private String name;
 
-    @NotEmpty (message = "Price cannot be empty!")
+    @NotEmpty(message = "Price cannot be empty!")
     private String price;
 
 }
